bed67e726c29189f956cddac5fb2b2a85366debf
Anjali Jain <80818092+Anjalijain-02@users.noreply.github.com>
2023-06-17 16:40:04 +0530

    Create fibbonacii_bottomup_with_space_optimization.java

---
+++ fibbonacii_bottomup_with_space_optimization.java
@@ -0,0 +1,27 @@
+public class Main
+{
+    public static int fib(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return n;
+        }
+         int a=0;
+         int b=1;
+         int c=0;
+        for(int i=2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+      
+           return c;
+      
+    }
+	public static void main(String[] args) {
+		int n=5;
+		
+	    System.out.println(fib(n));
+	}
+}
